2378893d74c640a093c3b6d765ad3151908da718
John Curcio <jmarrvin@gmail.com>
2017-02-04 13:23:16 -0500

    Expanded crossover, began move decision-making

--- animalClass.py
+++ animalClass.py
@@ -4,5 +4,6 @@ import numpy as np
 class Animal(object):
 
     def __init__(self, speed=5.0):
-        self.coords = [0.0, 0.0] #rho and theta, respectively
+        self.pos = [random.uniform(0.0, 2*np.pi), random.uniform(0.0, 2*np.pi)] 
+        # ^ rho and theta, respectively
         self.speed = speed
--- sheepClass.py
+++ sheepClass.py
@@ -7,14 +7,14 @@ import animalClass as ac
 class Sheep(ac.Animal):
 
     def __init__(self):
-        ac.Animal.__init__(self)
+        ac.Animal.__init__(self, 5)
+        self.age = 1 # yes, not zero. 
         self.strategy = Strategy()
 
 #This object contains a sheep's entire genetic material.
 class Strategy(object):
 
     def __init__(self, **kwargs):
-        sheep.age = 1
         self.mutationRate = 0.05
         if "mutationRate" in kwargs:
             self.mutationRate = kwargs["mutationRate"]
@@ -51,24 +51,60 @@ class Strategy(object):
                     break
         return mates
 
+    def move(sheepSet, wolf=None): #TODO: currently full of flaws and incomplete
+        vec = np.array([0.0, 0.0])
+        sheepSet = sheepSet.difference({self})
+        for sheep in sheepSet:
+            distVec = [min(x, 2*np.pi - x) for x in sheep.pos - self.pos] #TODO: incorrect. need value with minimum absolute value
+            dist = sum([x**2 for x in distVec])**0.5 
+            #this is the distance in angles, which is proportional to the length
+            #of the shortest path from two points on a sphere
+            n = len(self.sheepDistWeights)
+            weight = None
+            for i in range(n):
+                if dist >= 2*np.pi * i / n:
+                    weight = self.sheepDistWeights[i]
+                    break
+            vec += np.array([weight * x for x in (sheep.pos - self.pos)])
+        vec = np.linalg.norm(vec)
 
-def breed(*args): 
-    parents = args #there may be an arbitrary number of parents
+        wolfVec = np.array([0.0, 0.0])
+        if wolf != None:
+            n = len(self.wolfDistWeights)
+            for i in range(n):
+                if dist >= 2*np.pi * i / n:
+                    wolfVec += self.wolfDistWeights[i] * (self.pos - wolf.pos)
+            vec = self.sheepWeight * vec + self.wolfWeight * wolfVec
+            vec = np.linalg.norm(vec)
+        self.pos += self.speed * vec
+
+
+def breed(parents): 
+    #parents is a set of sheep
+    #there may be an arbitrary number of parents
     sheep3 = Sheep()
     nParents = len(parents)
-    for i in range(len(sheep3.strategy)):
+    for i in range(len(sheep3.sheepDistWeights)):
         #for each gene of the new strategy, randomly choose a subset of parents,
         #and set this gene to be the average of the i'th genes of the subset of
         #parents
         k = random.randrange(1, nParents)
         parentSubset = random.sample(parents, k)
-        sheep3.strategy[i] = 0.0
+        sheep3.sheepDistWeights[i] = 0.0
+        sheep3.wolfDistWeights[i] = 0.0
         for parent in parentSubset:
-            sheep3.strategy[i] += parent.strategy[i]
-        sheep3.strategy[i] /= k
+            sheep3.sheepDistWeights[i] += parent.sheepDistWeights[i]
+            sheep3.wolfDistWeights[i] += parent.wolfDistWeights[i]
+        sheep3.sheepDistWeights[i] /= k
+        sheep3.wolfDistWeights[i] /= k
         #mutations also occur, but their severity is determined by the 
         #mutationRate, not necessarily their frequency.
-        sheep3.strategy[i] += random.gauss(0.0, sheep3.strategy.mutationRate)
+        sheep3.sheepDistWeights[i] += random.gauss(0.0, sheep3.strategy.mutationRate)
+        sheep3.wolfDistWeights[i] += random.gauss(0.0, sheep3.strategy.mutationRate)
+    sheep3.sheepWeight += random.gauss(0.0, sheep3.strategy.mutationRate)
+    sheep3.sheepWeight = min(sheep3.sheepWeight, 1)
+    sheep3.sheepWeight = max(sheep3.sheepWeight, 0)
+    sheep3.wolfWeight = 1 - sheep3.sheepWeight
     return sheep3
 
 
